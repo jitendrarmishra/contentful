43498ada02d5674816cc45ce734310c941494198
Jitendra R Mishra <jitendrar.mishra@yahoo.com>
2024-10-13 16:15:15 +0530

    sixth commit

--- src/components/Aboutus.js
+++ src/components/Aboutus.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import client from '../contentfulClient';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
 const Aboutus = () => {
 
   const [data, setData] = useState([]);
@@ -11,7 +13,7 @@ const Aboutus = () => {
     const fetchData = async () => {
       try {
         const response = await client.getEntries({
-          content_type: 'blog', // Replace with your content type ID
+          content_type: 'aboutUs', // Replace with your content type ID
         });
         setData(response.items);
       
@@ -30,33 +32,16 @@ const Aboutus = () => {
 
 
   return (
+  
     <div className="container mt-4">
-      <h1>About Us</h1>
-      <p>
-      At My Blog, we are pioneers in artificial intelligence, dedicated to transforming the way businesses and individuals interact with technology. Founded in [Year], our mission is to harness the power of AI to drive innovation, enhance efficiency, and deliver intelligent solutions that empower our clients.
-      </p>
-      <p><strong>Our Vision</strong></p>
-      <p>
-      We envision a future where AI seamlessly integrates into everyday life, simplifying processes and unlocking new possibilities. Our commitment to ethical AI ensures that our technology is not only advanced but also responsible and aligned with the values of society.
-      </p>
-      <p><strong>Our Team</strong></p>
-      <p>
-      Our diverse team of experts—comprised of data scientists, engineers, and industry specialists—brings together a wealth of knowledge and experience. We collaborate closely with our clients to understand their unique challenges and tailor solutions that meet their needs.
-      </p>
-      <p><strong>What We Offer</strong></p>
-      <p>We specialize in a range of AI solutions, including:</p>
-      <p>
-        <ul>
-            <li>Machine Learning: Developing predictive models that drive smarter decision-making.</li>
-            <li>Natural Language Processing: Creating intuitive communication tools that enhance user interactions..</li>
-            <li>Computer Vision: Enabling machines to interpret and respond to visual data..</li>
-            <li>Robotics Automation: Streamlining operations through intelligent automation..</li>
-        </ul>
-      </p>
-      <p><strong>Why Choose Us?</strong></p>
-      <p>
-      With a focus on innovation, reliability, and customer satisfaction, we strive to exceed expectations. Our agile approach ensures that we remain at the forefront of AI advancements, continually adapting to the evolving landscape.
-      </p>
+  
+        {data.map((post) => (
+          <>
+          {console.log(post)}
+      <h1>{post.fields.title}</h1>
+      <div>{documentToReactComponents(post.fields.description)}</div>
+      </>
+        ))}
     </div>
   )
 }
